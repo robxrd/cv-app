4d2ad1f9d06220bc45c74936ad38084ad93d96d5
Rob <rob@debian.local>
2021-01-24 23:10:42 -0300

    .

---
+++ src/components/Avatar.js
@@ -0,0 +1,9 @@
+function Avatar() {
+    return(
+        <div className="avatar">
+            <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fderekwinnert.com%2Fwp-content%2Fuploads%2F2015%2F04%2F264.jpg&f=1&nofb=1" alt="avatar" className="image"/>
+        </div>
+    )
+}
+
+export default Avatar
